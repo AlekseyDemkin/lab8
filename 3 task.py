65bdb77a8a7f7ac607479eeffb8ff18d9fe10bc2
AlekseyDemkin <113420268+AlekseyDemkin@users.noreply.github.com>
2023-04-20 20:34:15 +0300

    Add files via upload

--- 3 task.py
+++ 3 task.py
@@ -4,6 +4,7 @@ import time
 cap = cv2.VideoCapture(0)  # захват картинки с видеокамеры
 down_points = (640, 480)  # изменяем размер изображения до 640х480 пикселей
 R, L, i = 0, 0, 0  # заводим переменные для счётчиков
+img = cv2.imread('fly64.png')
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -15,8 +16,10 @@ while True:
     ret, thresh = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     if len(contours) > 0:
-        c = max(contours, key=cv2.contourArea)  # поиск максимального контура
-        x, y, w, h = cv2.boundingRect(c)  # установка размеров найденного контура
+        c = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(c)
+        new_img = cv2.resize(img, (w, h))  # масштабируем изображение под найденный контур
+        frame[y:y + h, x:x + w] = new_img  # добавляем изображение на видео
         cv2.putText(frame, f"Count left: {L}", (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0),
                     1)  # вывод левого счётчика
         cv2.putText(frame, f"Count right: {R}", (down_points[0] - 180, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0),
@@ -29,7 +32,6 @@ while True:
                 R += 1
             elif a < down_points[0] // 2:
                 L += 1
-
     cv2.imshow('frame', frame)  # вывод изображения со всеми наложениями
     if cv2.waitKey(1) & 0xFF == ord('q'):  # закрывает программу, клавишей "q"
         break
